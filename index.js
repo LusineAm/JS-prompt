3f8006ff2598730c1a2310b923ce3ae04b88b270
Lusine Am <lusine.a.amiraghyan@gmail.com>
2020-07-15 20:13:35 +0400

    add work

---
+++ index.js
@@ -0,0 +1,5 @@
+const number1 = prompt('Գրեք առաջին թիվը');
+const number2 = prompt('Գրեք երկրորդ թիվը');
+const count = parseInt(number1) + parseInt(number2);
+alert( `${number1} + ${number2} = ${count}`);
+
